804a45ca172cf83b981520f72c9a54bec098779b
Joe Guida <joeguida@outlook.com>
2025-11-13 16:21:38 -0800

    move win32 input code to separate files

---
+++ include/platform/win32_input.hpp
@@ -0,0 +1,83 @@
+#ifndef GL_RENDERER_WIN32_INPUT_HPP
+#define GL_RENDERER_WIN32_INPUT_HPP
+
+#include <array>
+#include <functional>
+#include <optional>
+#include <unordered_map>
+
+#include "platform.hpp"
+#include "renderer.hpp"
+
+enum class ScanCode : USHORT {
+    A = 30,
+    B = 48,
+    C = 46,
+    D = 32,
+    E = 18,
+    F = 33,
+    G = 34,
+    H = 35,
+    I = 23,
+    J = 36,
+    K = 37,
+    L = 38,
+    M = 50,
+    N = 49,
+    O = 24,
+    P = 25,
+    Q = 16,
+    R = 19,
+    S = 31,
+    T = 20,
+    U = 22,
+    V = 47,
+    W = 17,
+    X = 45,
+    Y = 21,
+    Z = 44,
+    Escape = 1,
+};
+
+enum class KeyCode : USHORT {
+    Forward,
+    Back,
+    Left,
+    Right,
+    Quit,
+};
+
+enum class KeyState {
+    Up,
+    Down,
+};
+
+static std::unordered_map<ScanCode, KeyCode> key_bindings {
+    { ScanCode::W, KeyCode::Forward },
+    { ScanCode::S, KeyCode::Back },
+    { ScanCode::A, KeyCode::Left },
+    { ScanCode::D, KeyCode::Right },
+    { ScanCode::Escape, KeyCode::Quit }
+};
+
+static std::unordered_map<KeyCode, KeyState> key_states {
+    { KeyCode::Forward, KeyState::Up },
+    { KeyCode::Back, KeyState::Up },
+    { KeyCode::Left, KeyState::Up },
+    { KeyCode::Right, KeyState::Up },
+    { KeyCode::Quit, KeyState::Up }
+};
+
+struct IInput {
+    std::unordered_map<KeyCode, std::function<void()>> input_map;
+    std::unordered_map<ScanCode, KeyCode> key_map;
+    std::array<RAWINPUTDEVICE, 2> devices;
+
+    void bind(KeyCode key, const std::function<void()>& function);
+    std::optional<KeyCode> get_keycode(ScanCode scancode);
+    void setup_input_devices(HWND hwnd);
+    void handle_inputs(LPARAM lparam, HWND hwnd, Renderer& renderer);
+    void keypress(KeyCode key, KeyState state);
+};
+
+#endif
---
+++ src/platform/win32_input.cpp
@@ -0,0 +1,80 @@
+#include "platform/win32_input.hpp"
+
+#include <vector>
+
+#include <spdlog/spdlog.h>
+
+void IInput::bind(KeyCode key, const std::function<void()>& function) {
+    input_map[key] = function;
+}
+
+std::optional<KeyCode> IInput::get_keycode(ScanCode scancode) {
+    if(key_bindings.contains(scancode)) {
+        return key_bindings[scancode];
+    }
+
+    return std::nullopt;
+}
+
+void IInput::setup_input_devices(HWND hwnd) {
+    devices[0] = {
+        .usUsagePage = 0x01, // HID_USAGE_PAGE_GENERIC
+        .usUsage = 0x06,     // HID_USAGE_GENERIC_KEYBOARD
+        .dwFlags = 0,
+        .hwndTarget = hwnd
+    };
+
+    devices[1] = {
+        .usUsagePage = 0x01, // HID_USAGE_PAGE_GENERIC
+        .usUsage = 0x02,     // HID_USAGE_GENERIC_MOUSE
+        .dwFlags = 0,
+        .hwndTarget = hwnd
+    };
+
+    if(RegisterRawInputDevices(devices.data(), devices.size(), sizeof(RAWINPUTDEVICE)) == FALSE) {
+        spdlog::error("RegisterRawInputDevices failed!"); 
+    }
+}
+
+void IInput::handle_inputs(LPARAM lparam, HWND hwnd, Renderer& renderer) {
+    UINT dw_size;
+    if(GetRawInputData((HRAWINPUT)lparam, RID_INPUT, NULL, &dw_size, sizeof(RAWINPUTHEADER)) == -1) {
+        spdlog::error("GetRawInputData failed");
+        return;
+    }
+
+    std::vector<BYTE> bytes;
+    if(GetRawInputData((HRAWINPUT)lparam, RID_INPUT, bytes.data(), &dw_size, sizeof(RAWINPUTHEADER)) != dw_size) {
+        spdlog::error("GetRawInputData returning incorrect size");
+        return;
+    }
+
+    RAWINPUT* raw = reinterpret_cast<RAWINPUT*>(bytes.data());
+
+    if(!raw) {
+        spdlog::error("RAWINPUT* raw is null");
+        return;
+    }
+
+    if(raw->header.dwType == RIM_TYPEKEYBOARD) {
+        ScanCode scancode = static_cast<ScanCode>(raw->data.keyboard.MakeCode);
+        std::optional<KeyCode> keycode = get_keycode(scancode);
+        KeyState state;
+
+        if(raw->data.keyboard.Flags == RI_KEY_MAKE) {
+            state = KeyState::Down;
+        } 
+        else if(raw->data.keyboard.Flags == RI_KEY_BREAK) {
+            state = KeyState::Up;
+        }
+
+        if(keycode.has_value()) {
+            keypress(keycode.value(), state);
+        }
+    }
+}
+
+void IInput::keypress(KeyCode key, KeyState state) {
+    key_states[key] = state;
+}
+
